d839e0f6d45e723714c6b01172f4f7d2e1e95813
nehemiatelusma <nehemia.telusma@akqa.com>
2025-12-18 18:05:12 +0100

    fix: using dvh for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Nav from "@/components/nav";
 
 export default function Home() {
   return (
-    <main className="bg-gradient-to-b from-dark to-light flex flex-col justify-between h-screen overflow-hidden">
+    <main className="bg-gradient-to-b from-dark to-light flex flex-col justify-between h-dvh overflow-hidden">
       <Intro />
       <Nav />
       <RandomSongStack />
