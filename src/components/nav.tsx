ba7f41f075594e31fd9012474b2cac7eb788214b
nehemiatelusma <nehemia.telusma@akqa.com>
2025-12-20 00:41:15 +0100

    fix: new nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,12 +2,8 @@ import Spotlight from "./spotlight";
 
 const Nav = () => {
     return (
-        <nav className="fixed top-8 left-0 px-8 w-full z-50">
-            <ul className="grid grid-cols-3 items-center justify-between w-full">
-                <li></li>
-                <li className="flex justify-center items-center text-base lg:text-xl text-zinc-700 font-maghfirea cursor-pointer self-center text-center">Meryl Edition</li>
-                <li className="flex justify-end items-center text-sm text-zinc-700 font-maghfirea cursor-pointer"><a href="https://www.tiktok.com/@ici_miiadev" target="_blank">[@ici_miiadev]</a></li>
-            </ul>
+        <nav className="fixed top-8 left-0 px-8 w-full z-50 flex justify-center items-center">
+            <h1 className="flex justify-center items-center text-2xl lg:text-4xl text-zinc-700 font-maghfirea cursor-pointer self-center text-center">Meryl Edition</h1>
         </nav>
     )
 }
