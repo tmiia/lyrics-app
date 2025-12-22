f3287b5a2c631e884e613b068f46d9fc59e4645f
nehemiatelusma <nehemia.telusma@akqa.com>
2025-12-22 15:17:58 +0100

    feat: add cover texture to middle part of vinyl

--- src/components/Experience/vinylRecord.tsx
+++ src/components/Experience/vinylRecord.tsx
@@ -1,9 +1,10 @@
 import { usePlayer } from '@/context/PlayerContext'
 import { useLoader } from '@react-three/fiber'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
+import { useTexture } from '@react-three/drei'
 
 type VinylRecordProps = {
   position?: [number, number, number]
@@ -12,8 +13,23 @@ type VinylRecordProps = {
 }
 
 const VinylRecord = ({ position = [0, 0, 0], rotation = [0, 0, 0], scale = [1, 1, 1] }: VinylRecordProps) => {
-  const gltf = useLoader(GLTFLoader, '/models/12_vinyl_record_v2.glb')
+  const gltf = useLoader(GLTFLoader, '/models/12_vinyl_record_v3.glb')
   const { state } = usePlayer()
+  const coverTexture = useTexture(state.currentTrack?.cover || '/covers/default_cover.jpg')
+  
+
+  useEffect(() => {
+    if (gltf.scene && coverTexture) {
+      gltf.scene.traverse((child) => {
+        if (child instanceof THREE.Mesh && child.name === 'extern') {
+          coverTexture.repeat.set(1, 1)
+          coverTexture.center.set(0.5, 0.5)
+          child.material.map = coverTexture
+        }
+      })
+    }
+  }, [gltf.scene, coverTexture])
+  
   const groupRef = useRef<THREE.Group>(null)
   const velocityRef = useRef(0)
 
